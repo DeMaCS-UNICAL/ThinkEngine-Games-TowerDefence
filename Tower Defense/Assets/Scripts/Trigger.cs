4ab69dc8b9a6b375fd4fec50b30259ce82a8ef95
Salvatore Laiso <salvatorelaiso@outlook.it>
2020-12-02 17:47:47 +0100

    Removed project based on non-LTS Unity version

--- Tower Defense/Assets/Scripts/Trigger.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-// every method of this class returning a bool value can be used to trigger the sensors update.
-public class Trigger : ScriptableObject
-{
-}
--- Tower Defense/README.md
+++
@@ -1,2 +0,0 @@
-﻿# Tower-Defense
-##### based on [Tower-Defense-Tutorial](https://github.com/Brackeys/Tower-Defense-Tutorial) from [Brackeys](https://github.com/Brackeys)
